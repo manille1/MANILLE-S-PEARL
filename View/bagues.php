27ddc1b933422e425fd4fac483038d914baed838
Manille Desseaux <manilledesseaux@gmail.com>
2025-01-14 09:10:21 +0100

    petite avancée sur view bagues

--- View/bagues.php
+++ View/bagues.php
@@ -36,3 +36,15 @@
         color: whitesmoke;
     }
 </style>
+
+<script src="./assets/js/services/article.js" type="module"></script>
+<script src="./assets/js/components/article.js" type="module"></script>
+<script type ="module">
+    import { refreshList } from "./assets/js/components/article.js";
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        let currentPage = 1
+
+        refreshList(currentPage)
+    })
+</script>
